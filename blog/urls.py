5d2becd5631edbdfd83703ef3a5a2ced45212b64
alexey <alex_tagan@mail.ru>
2016-10-21 15:34:42 +0300

    Add Brython button in main menu site

--- blog/urls.py
+++ blog/urls.py
@@ -22,5 +22,6 @@ from . import views
 urlpatterns = (
     url(r'^$', views.home, name='home'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^articles/(?P<article_id>[0-9]+)/$', views.show_article, name='article')
+    url(r'^articles/(?P<article_id>[0-9]+)/$', views.show_article, name='article'),
+    url(r'^testbrython/$', views.test_brython, name='testbrython')
 )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Article
 
+
 # Take the data from the database and render an HTML page from the template
 
 
@@ -17,3 +18,7 @@ def about(request):
 def show_article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     return render(request, 'blog/article.html', {'article': article})
+
+
+def test_brython(reqest):
+    return render(reqest, 'blog/testbrython.html')
